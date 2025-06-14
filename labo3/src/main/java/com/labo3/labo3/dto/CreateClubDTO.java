70f80212d8da7ef5d0b842f334739942ba03057f
Alvaradox356 <00144318@uca.edu.sv>
2025-06-14 09:40:17 -0600

    adding dtos

--- labo3/src/main/java/com/labo3/labo3/dto/CreateClubDTO.java
+++ labo3/src/main/java/com/labo3/labo3/dto/CreateClubDTO.java
@@ -1,4 +1,18 @@
 package com.labo3.labo3.dto;
 
+import jakarta.validation.constraints.NotEmpty;
+import lombok.Data;
+import lombok.Getter;
+
+@Getter
+@Data
 public class CreateClubDTO {
+    @NotEmpty
+    private String clubName;
+    @NotEmpty
+    private String country;
+    @NotEmpty
+    private String coach;
+    @NotEmpty
+    private String titles;
 }
--- labo3/src/main/java/com/labo3/labo3/repositories/ClubRepository.java
+++ labo3/src/main/java/com/labo3/labo3/repositories/ClubRepository.java
@@ -1,4 +1,9 @@
 package com.labo3.labo3.repositories;
 
-public class ClubRepository {
+import com.labo3.labo3.entities.Club;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClubRepository extends JpaRepository<Club, Integer> {
 }
