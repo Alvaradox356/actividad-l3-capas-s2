ac6896eda8474774d1e6b4166ca78decc677b9d6
PedroManzanares14 <00230422@uca.edu.sv>
2025-06-14 09:56:51 -0600

    controller

---
+++ labo3/src/main/java/com/labo3/labo3/controller/ClubController.java
@@ -0,0 +1,24 @@
+package com.labo3.labo3.controller;
+
+import com.labo3.labo3.dto.CreateClubDTO;
+import com.labo3.labo3.services.ClubService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/clubs")
+public class ClubController {
+
+    @Autowired
+    private ClubService clubService;
+
+    // Endpoint para registrar un club
+    @PostMapping
+    public ResponseEntity<String> createClub(@RequestBody @Valid CreateClubDTO createClubDTO) {
+        clubService.addClub(createClubDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body("Club registrado exitosamente");
+    }
+}
